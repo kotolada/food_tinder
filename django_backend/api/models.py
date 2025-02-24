e614f417bdfa5278773ba135a17eeab5312373bd
Lada <kotolada1@gmail.com>
2025-02-24 15:35:54 +0000

    Minor model update

--- django_backend/api/models.py
+++ django_backend/api/models.py
@@ -4,7 +4,7 @@ from django.db import models
 class Recipe(models.Model):
     name = models.CharField(max_length=100, blank=False, null=False)
     description = models.CharField(max_length=1000, blank=True, null=True)
-    ingredients = models.JSONField(default=list)
+    ingredients = models.CharField(max_length=1000, blank=False, null=False)
 
 
     def __str__(self):
