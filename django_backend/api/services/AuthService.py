388f800be2668234a1ab0000ce06d6a7183864dc
Lada <kotolada1@gmail.com>
2025-02-27 21:28:26 +0000

    Comment

--- django_backend/api/services/AuthService.py
+++ django_backend/api/services/AuthService.py
@@ -5,4 +5,4 @@ class AuthService:
     def create_by_creds(data: CreateUserSerializer):
         data.is_valid(raise_exception=True)
         data.save()
-        return data
+        return data # comment
